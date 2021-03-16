4e539f196254017eb83f72f3ec41890640a60ea6
growinghsb <seungbeombm@gmail.com>
2021-03-16 12:58:44 +0900

    Add Car class to move method and test code

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,11 @@ public class Car {
     }
 
     // 추가 기능 구현
+
+    public int move(int random) {
+        if (random >= 4) {
+            return position + 1;
+        }
+        return position;
+    }
 }
--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -10,4 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
 
+    @Test
+    public void Car객체_Move_Test() {
+        //given (~ 가정했을 때)
+        Car car = new Car("test");
+
+        //when (~ 할 때), then (그렇다면)
+        int position = car.move(3);
+        assertThat(position).isZero();
+
+        position = car.move(4);
+        assertThat(position).isEqualTo(1);
+    }
+
 }
