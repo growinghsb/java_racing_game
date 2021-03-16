7bb8b1ab85e5546870c7477994fa0530eb01695e
growinghsb <seungbeombm@gmail.com>
2021-03-16 13:10:38 +0900

    Add Application class to createCars method

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
@@ -7,6 +9,16 @@ public class Application {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
 
+    }
+
+    public static Map<String, Car> createCars(String[] carNames) {
+        Map<String, Car> cars = new HashMap<>();
+
+        for (String carName : carNames) {
+            cars.put(carName, new Car(carName));
+        }
+
+        return cars;
     }
 
     private static String[] inputCarNames(Scanner scanner){
