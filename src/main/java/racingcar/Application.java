2443f0e5cb4d9ebf73580e7b74ed577e9e22a11a
growinghsb <seungbeombm@gmail.com>
2021-03-16 12:35:32 +0900

    add input carName in scanner

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,5 +6,10 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+
+    }
+
+    private static String[] inputCarNames(Scanner scanner){
+        return scanner.next().split(",");
     }
 }
---
+++ src/test/java/racingcar/ApplicationTest.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationTest {
+
+}
