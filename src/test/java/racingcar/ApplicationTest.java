c3b0a133ab30d8397fd04ca3ec056aa4f7daa305
growinghsb <seungbeombm@gmail.com>
2021-03-16 13:17:22 +0900

    Add test to createCars Method

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -3,10 +3,12 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static racingcar.Application.*;
 
 class ApplicationTest {
 
@@ -21,6 +23,16 @@ class ApplicationTest {
 
         position = car.move(4);
         assertThat(position).isEqualTo(1);
+    }
+
+    @Test
+    public void createCar_Method_Test() {
+        //given (~ 가정했을 때)
+        Map<String, Car> cars = createCars(new String[]{"sb", "hsb"});
+
+        //when (~ 할 때), then (그렇다면)
+        assertThat(cars.get("sb")).isNotNull();
+        assertThat(cars.get("hsb")).isNotNull();
     }
 
 }
